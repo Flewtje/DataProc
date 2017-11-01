d679a9ca397fd282a514caeef39979b5ed4c4352
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-01 19:24:27 +0100

    crawler: added writer, fixed runtime

--- Homework/crawling/crawler.py
+++ Homework/crawling/crawler.py
@@ -15,19 +15,31 @@ SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0]
 BACKUP_DIR = os.path.join(SCRIPT_DIR, 'HTML_BACKUPS')
 
 def page_parser(data):
-    URL = ''.join((BASE_URL, data['link']))
+
+    # create url for individual movie and parse with bs4
+    URL = BASE_URL + data['link']
     html = urllib.request.urlopen(URL).read()
+
+    # use standard python html parser
     soup = BeautifulSoup(html, 'html.parser')
     
+    # find data from movie page modify it and store it in a dictionary
+    data['runtime'] = soup.find('time', attrs={'itemprop': 'duration'})['datetime'][2:-1]    
     sub_text = soup.find('div', 'subtext')
-    data['runtime'] = sub_text.time.string.strip()
-    #for genre in sub_text.find_all('span')
+    
+    # get the genres
     data['genre'] = []
-    for el in sub_text.find_all('span', 'itemprop'):
+    for el in sub_text('span', 'itemprop'):
         data['genre'].append(el.string.strip())
     data['genre'] = ';'.join(data['genre'])
-    #plot_summary = soup.find('div', 'plot_summary')
-    #data['director'] = plot_summary.find('director').find('span', 'itemprop').string.strip()
+
+    # not working yet
+    data['writer'] = []
+    for el in soup('span', attrs={'itemprop': 'creator', 'itemtype': 'http://schema.org/Person'}):
+        data['writer'].append(el.find('a').string.strip())
+
+    data['director'] = []
+
     return data
 
 if __name__ == '__main__':
@@ -36,7 +48,7 @@ if __name__ == '__main__':
     html = urllib.request.urlopen(TARGET_URL).read()
     soup = BeautifulSoup(html, 'html.parser')
     movies = []
-    for movie in soup.find_all('td', 'titleColumn'):
+    for movie in soup('td', 'titleColumn'):
         data = {}
         data['title'] = movie.a.string
         data['link'] = movie.a.get('href')
