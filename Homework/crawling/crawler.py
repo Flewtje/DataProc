838a7b8031a3b4a915d6b65b4dc697caaa9dc8a0
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-01 21:35:50 +0100

    crawler complete

--- Homework/crawling/crawler.py
+++ Homework/crawling/crawler.py
@@ -11,21 +11,31 @@ import os
 # constants
 TARGET_URL = 'http://www.imdb.com/chart/top'
 BASE_URL = 'http://www.imdb.com/'
-OUTPUT_CSV = 'top250movies.csv'
+OUTPUT_CSV = 'topmovies.csv'
 SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0]
 BACKUP_DIR = os.path.join(SCRIPT_DIR, 'HTML_BACKUPS')
+
+def retrieve_html(url):
+    html = None
+    while not html:
+        try:
+            html = urllib.request.urlopen(url, timeout=10).read()
+        except:
+            print("Download failed, will retry")
+
+
+    return html
 
 def page_parser(data):
 
     # create url for individual movie and parse with bs4
-    URL = BASE_URL + data['link']
-    html = urllib.request.urlopen(URL).read()
 
     # use standard python html parser
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(retrieve_html(BASE_URL + data['link']), 'html.parser')
     
     # find data from movie page modify it and store it in a dictionary
-    data['runtime'] = soup.find('time', attrs={'itemprop': 'duration'})['datetime'][2:-1]    
+    data['runtime'] = soup.find('time', attrs={'itemprop': 'duration'})\
+    ['datetime'][2:-1]    
     sub_text = soup.find('div', 'subtext')
     
     # get the genres
@@ -36,7 +46,8 @@ def page_parser(data):
 
     # find writers
     data['writer'] = []
-    for el in soup('span', attrs={'itemprop': 'creator', 'itemtype': 'http://schema.org/Person'}):
+    for el in soup('span', attrs={'itemprop': 'creator', 'itemtype':\
+     'http://schema.org/Person'}):
         data['writer'].append(el.find('a').string.strip())
     data['writer'] = ';'.join(data['writer'])
 
@@ -50,14 +61,19 @@ def page_parser(data):
         data['actor'].append(el.find('span').string.strip())
     data['actor'] = ';'.join(data['actor'])
 
-    rating_string = soup.find('div', class_='ratingValue').find('strong')['title']
+    rating_string = soup.find('div', class_='ratingValue').find('strong')\
+    ['title']
     data['rating'] = rating_string[:3]
 
-    # regular expression used to remove all non digits from place 3 onwards
+    # regular expression used to remove all non-digits from place 3 onwards
     data['voters'] = re.sub('[^0-9]', '', rating_string[3:])
 
-
     return data
+
+def csv_storage(output_file, movies):
+    writer = csv.writer(output_file)
+    #for movie in movies:
+    writer.writerow([movie['title'], movie['runtime'], movie['genre'], movie['director'], movie['writer'], movie['actor'], movie['rating'], movie['voters']])
 
 if __name__ == '__main__':
 
@@ -65,10 +81,18 @@ if __name__ == '__main__':
     html = urllib.request.urlopen(TARGET_URL).read()
     soup = BeautifulSoup(html, 'html.parser')
     movies = []
-    for movie in soup('td', 'titleColumn'):
+    with open(OUTPUT_CSV, 'w') as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(['Title', 'Runtime', 'Genre(s)', 'Director(s)', 'Writer(s)', 'Actor(s)', 'Rating', 'Number of ratings'])
+    
+    for i, movie in enumerate(soup('td', 'titleColumn')[:260]):
         data = {}
         data['title'] = movie.a.string
         data['link'] = movie.a.get('href')
         data = page_parser(data)
-        print(data)
-        movies.append(data)
+        print(i)
+        with open(OUTPUT_CSV, 'a') as output_file:
+            writer = csv.writer(output_file)
+            writer.writerow([data['title'], data['runtime'], data['genre'],\
+                data['director'], data['writer'], data['actor'], \
+                data['rating'], data['voters']])
