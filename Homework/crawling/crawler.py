b39f2043df2cb94a2b8641365fe2b2daa157ab07
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-01 22:13:47 +0100

    made some comments, cleaned some code

--- Homework/crawling/crawler.py
+++ Homework/crawling/crawler.py
@@ -2,6 +2,7 @@
 # Sebastiaan Arendsen
 # 
 
+# using bs4 instead of patterns as it allows python3
 from bs4 import BeautifulSoup
 import re
 import urllib.request
@@ -12,35 +13,33 @@ import os
 TARGET_URL = 'http://www.imdb.com/chart/top'
 BASE_URL = 'http://www.imdb.com/'
 OUTPUT_CSV = 'topmovies.csv'
-SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0]
-BACKUP_DIR = os.path.join(SCRIPT_DIR, 'HTML_BACKUPS')
 
 def retrieve_html(url):
+    """
+    Function created to retrieve a url using urllib. Problems might arise when
+    page takes to long to react, so timeout is build in with an error message.
+    """
     html = None
     while not html:
         try:
             html = urllib.request.urlopen(url, timeout=10).read()
         except:
-            print("Download failed, will retry")
-
+            print('Download failed, will retry.')
 
     return html
 
 def page_parser(data):
 
-    # create url for individual movie and parse with bs4
-
-    # use standard python html parser
+    # use standard python html parser and bs4
     soup = BeautifulSoup(retrieve_html(BASE_URL + data['link']), 'html.parser')
     
     # find data from movie page modify it and store it in a dictionary
     data['runtime'] = soup.find('time', attrs={'itemprop': 'duration'})\
     ['datetime'][2:-1]    
-    sub_text = soup.find('div', 'subtext')
     
     # get the genres
     data['genre'] = []
-    for el in sub_text('span', 'itemprop'):
+    for el in soup.find('div', 'subtext').find_all('span', 'itemprop'):
         data['genre'].append(el.string.strip())
     data['genre'] = ';'.join(data['genre'])
 
@@ -51,46 +50,56 @@ def page_parser(data):
         data['writer'].append(el.find('a').string.strip())
     data['writer'] = ';'.join(data['writer'])
 
+    # find directors
     data['director'] = []
     for el in soup('span', attrs={'itemprop': 'director'}):
         data['director'].append(el.find('a').string.strip())
     data['director'] = ';'.join(data['director'])
 
+    # find actors
     data['actor'] = []
-    for el in soup('span', attrs={'itemprop': 'actors'}):
+    for el in soup('span', attrs={'itemprop': 'actors'})[:3]:
         data['actor'].append(el.find('span').string.strip())
     data['actor'] = ';'.join(data['actor'])
 
+    # get the rating from the soup
     rating_string = soup.find('div', class_='ratingValue').find('strong')\
     ['title']
+
+    # first three chars are the actual rating
     data['rating'] = rating_string[:3]
 
     # regular expression used to remove all non-digits from place 3 onwards
+    # this is needed for the amount of voters as it is obscured by non-digits
     data['voters'] = re.sub('[^0-9]', '', rating_string[3:])
 
+    # all the data has been collected
     return data
 
-def csv_storage(output_file, movies):
-    writer = csv.writer(output_file)
-    #for movie in movies:
-    writer.writerow([movie['title'], movie['runtime'], movie['genre'], movie['director'], movie['writer'], movie['actor'], movie['rating'], movie['voters']])
-
+# main function
 if __name__ == '__main__':
 
     # get soup for imdb top 250 page
-    html = urllib.request.urlopen(TARGET_URL).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    movies = []
+    soup = BeautifulSoup(retrieve_html(TARGET_URL), 'html.parser')
+    
+    # write first line in csv file
     with open(OUTPUT_CSV, 'w') as output_file:
         writer = csv.writer(output_file)
         writer.writerow(['Title', 'Runtime', 'Genre(s)', 'Director(s)', 'Writer(s)', 'Actor(s)', 'Rating', 'Number of ratings'])
     
-    for i, movie in enumerate(soup('td', 'titleColumn')[:260]):
+    # iterate over all movies in the top 250
+    for i, movie in enumerate(soup('td', 'titleColumn')):
         data = {}
         data['title'] = movie.a.string
+        
+        # link is stored and individual page is parsed
         data['link'] = movie.a.get('href')
         data = page_parser(data)
-        print(i)
+        
+        # give an indication of the progress, as it might take a long time
+        print('Current entry: {} {}'.format(i + 1, data['title']))
+        
+        # write the data to the csv file
         with open(OUTPUT_CSV, 'a') as output_file:
             writer = csv.writer(output_file)
             writer.writerow([data['title'], data['runtime'], data['genre'],\
