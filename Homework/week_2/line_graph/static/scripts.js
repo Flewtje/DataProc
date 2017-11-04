638eff8d415932b185457fc5cb50bb0656d15278
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-04 18:38:59 +0100

    javascript loading

---
+++ Homework/week_2/line_graph/static/scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+// Sebastiaan Arendsen
+// 6060072
+
+document.addEventListener('DOMContentLoaded', function(){     
+    
+    var canvas = document.getElementById('graph');
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'rgb(0,0,200)';
+    ctx.fillRect(10, 10, 55, 50);
+
+});
