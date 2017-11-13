fc14640b47b8773a9c9b770d664397de843c7b53
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-13 17:39:54 +0100

    got bar graph

--- Homework/week_3/static/scripts.js
+++ Homework/week_3/static/scripts.js
@@ -4,20 +4,47 @@
  */
 
 'use strict';
-var DATA_URL = 'data/pirates_temperature.json';
 
+// set data and margins and sizes
+var DATA_URL = 'data/pirates_temperature.json';
+var margin = {top: 10, right: 20, bottom: 30, left: 40};
+var height = 400;
+var barWidth = 100;
+
+// triggers when document is fully loades
 document.addEventListener('DOMContentLoaded', function() {
+
+    // get json and draw graph
     d3.json(DATA_URL, function(json) {
         drawBarGraph(json);
-    })
+    });
 })
 
 function drawBarGraph(json) {
-    var width = 600
-        barHeight = 50;
-    var chart = d3.select('.chart')
-        .attr('width', width)
-        .attr('height', barHeight * json.length);
-    
-    var x = d3.scaleLinear()
+
+    // set x-scaling
+    var x = d3.scaleBand().rangeRound([0, barWidth * json.length]).padding(0.2)
+        .domain(json.map(function(d) { return d.year; }));
+
+    // set y-scaling
+    var y = d3.scaleLinear().rangeRound([height, 0])
+        .domain([0, d3.max(json, function(d) { return d['number of pirates'] })]);
+
+    // select svg chart and set its width and height
+    var chart = d3.select('svg')
+        .attr('width', barWidth * json.length + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom );
+
+    // add 
+    var g = chart.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    g.selectAll('.chart')
+        .data(json)
+        .enter().append('rect')
+        .attr('class', 'chart')
+        .attr('x', function(d) { return x(d.year); })
+        .attr('y', function(d) { return y(d['number of pirates']); })
+        .attr('width', x.bandwidth())
+        .attr('height', function(d) { return height - y(d['number of pirates']); });
 }
