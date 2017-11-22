802a449f68d7ea375c316834b5a9bf8d71c62b57
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-11-22 15:09:26 +0100

    comments and everything done

--- Homework/week_4/static/scripts.js
+++ Homework/week_4/static/scripts.js
@@ -1,10 +1,16 @@
 /* Sebastiaan Arendsen
  * 6060072
  * scripts.js
+ * 
+ * This script creates a d3 scatterplot based on two fairly non-compatible data
+ * sets. While it's probably inefficient to try to correct this in the js,
+ * it makes for a nice exercise. Some pre-processing of the data would 
+ * probably lead to a much cleaner code and more general applicability.
  */
 
 'use strict';
 
+// 'Magic Numbers'
 var margin = {top: 50, right: 30, bottom: 40, left: 200},
     height = 500 - margin.top - margin.bottom,
     width = 960 - margin.left - margin.right,
@@ -30,11 +36,23 @@ var margin = {top: 50, right: 30, bottom: 40, left: 200},
         buddhism: 'BUDGEN',
         hinduism: 'HINDGEN'
     },
+    labels = ['islam', 'christianity', 'judaism', 'buddhism', 'hinduism', 'other'], 
     xLabelText = 'GDP per capita',
     yLabelText = 'Religious perunage',
-    titleOfPlot = 'Religion vs GDP';
+    titleOfPlot = 'Religion vs GDP',
+    legendSpacing = 25,
+    legendOffset = 20,
+    legendBox = 20,
+    legendTextOffset = 42,
+    tipOffset = 3,
+    popDiv = 3000000,
+    legendTopText = 'Dominant religion',
+    legendBotText = 'Size represents population';
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    // set document title
+    document.title = titleOfPlot;
 
     // make a queue to load both datasets before executing code
     var q = d3.queue()
@@ -45,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .await(function(error, json_religion, json_gdp) {
             if (error) throw error;
 
+            // merge data lists
             var json = d3.merge([json_gdp, json_religion]);
 
             // nest the data under the country code
@@ -59,11 +78,14 @@ document.addEventListener('DOMContentLoaded', function() {
                 })
                 .entries(json);
 
+            // remove non-existant data
             for (var i = (nested_json.length - 1); i >= 0; i--) {
                 if (nested_json[i].values[0][year] == undefined || nested_json[i].values[0][year].length === 0) {
                     nested_json.splice(i, 1);
                 }
             }
+
+            // draw the graph
             drawScatterPlot(nested_json, year);
         });
 });
@@ -76,60 +98,66 @@ function drawScatterPlot(data, year) {
         .range([0, width])
         .domain(d3.extent(data, function(d) { return parseFloat(d.values[0][year]); }));
 
+    // y will be the religionpct
     var y = d3.scaleLinear()
         .range([height, 0])
         .domain(d3.extent(data, function(d) {
+
+            // religion data is stored in different objects per year, so has to be found
             for (var i = 1; i < d.values.length; i++) {
                 if (d.values[i]['YEAR'] == year) {
                     return (1.0 - parseFloat(d.values[i]['NORELPCT'].replace(',', '.')));
                 }
             }
-            return undefined; 
         }));
 
+    // instantiate tooltip
     var tip = d3.tip()
         .attr('class', 'd3-tip')
-        .offset([3, 0])
+        .offset([tipOffset, 0])
         .direction('s')
+
+        // tooltip will show country name
         .html(function(d) {
             return d.values[0]['Country Name'];
         });
 
+    // set width and height of svg and call tip on the svg
     var svg = d3.select('svg')
         .attr('height', height + margin.top + margin.bottom)
         .attr('width', width + margin.left + margin.right)
         .call(tip);
 
+    // append group to svg for actual chart
     var chart = svg.append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+    // create x-axis
     var xAxis = chart.append('g')
         .attr('class', 'x axis')
         .attr('transform', 'translate(0,' + height + ')')
         .call(d3.axisBottom(x));
 
+    // create y-axis
     chart.append('g')
         .attr('class', 'y axis')
-        .call(d3.axisLeft(y))
-    .append('text')
-        .attr('class', 'label')
-        .attr('transform', 'rotate(-90)')
-        .attr('y', 6)
-        .attr('dy', '.71em')
-        .style('text-anchor', 'end')
-        .text('Perecentage religious');
+        .call(d3.axisLeft(y));
 
+    // append actual data as dots and circles
     chart.selectAll('.dot')
         .data(data)
     .enter().append('circle')
         .attr('class', 'dot')
 
+        // fill the dots based on dominant religion
         .attr('fill', function(d) {
             for (var i = 0; i < d.values.length; i++) {
                 if (d.values[i]['YEAR'] == year) {
-                    var color = colors.other;
+                    var color;
                     var total = 0;
                     var highest = 0;
+
+                    // check which religion is most dominant and store it's color
                     if (parseFloat(d.values[i][religions.christianity].replace(/,/g, '')) > highest) {
                         color = colors.christianity;
                         highest = parseFloat(d.values[i][religions.christianity].replace(/,/g, ''));
@@ -155,16 +183,18 @@ function drawScatterPlot(data, year) {
                         highest = parseFloat(d.values[i][religions.hinduism].replace(/,/g, ''));
                         total += highest;
                     }
+
+                    // get population number for final check
                     for (var i = 1; i < d.values.length; i++) {
                         if (d.values[i]['YEAR'] == year) {
                             var pop =  parseFloat(d.values[i]['POP'].replace(/,/g, ''));
                         }
                     }
 
+                    // set other if highest is smaller than total pop - total counted religions
                     if (highest > (pop - total)) {
-                        return color;                   
+                        return color;
                     }
-
                     else {
                         return colors.other;
                     }
@@ -172,16 +202,22 @@ function drawScatterPlot(data, year) {
             }
         })
 
-        // determine 'area' based on population
+        // determine area of dot based on population
         .attr('r', function(d) {
             for (var i = 1; i < d.values.length; i++) {
                 if (d.values[i]['YEAR'] == year) {
-                    return Math.sqrt(parseFloat(d.values[i]['POP'].replace(/,/g, '')) / 3000000);
+                    return Math.sqrt(parseFloat(d.values[i]['POP'].replace(/,/g, '')) / popDiv);
                 }
             }
         })
+
+        // x pos based on gdp
         .attr('cx', function(d) { return x(parseFloat(d.values[0][year])); })
+
+        // y pos based on religion
         .attr('cy', function(d) {
+
+            // religion is buried within an array of objects id'ed by year
             for (var i = 1; i < d.values.length; i++) {
                 if (d.values[i]['YEAR'] == year) {
                     return y(1.0 - parseFloat(d.values[i]['NORELPCT'].replace(',', '.')));
@@ -189,9 +225,12 @@ function drawScatterPlot(data, year) {
             }
             return undefined; 
         })
+
+        // add mouseover events for tooltip to data points
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide);
 
+        // add labels to x and y-axis
         var xLabel = chart.append('text')
             .attr('class', 'label')
             .attr('transform', 'translate(' + width + ',' + (height + margin.bottom - 5) + ')')
@@ -200,39 +239,55 @@ function drawScatterPlot(data, year) {
 
         var yLabel = svg.append('text')
             .attr('class', 'label')
+
+            // rotate and translate the y-axis
             .attr('transform', 'rotate(-90) translate(' + (margin.top - 220) + ',' + (margin.left - 30) + ')')
             .attr('text-anchor', 'begin')
             .text(yLabelText);
 
+        // append group for legend and place it slightly from the edge
         var legend = svg.append('g')
             .attr('transform', 'translate(4,' + margin.top + ')');
-
-        var labels = ['islam', 'christianity', 'judaism', 'buddhism', 'hinduism', 'other'] 
         
-        var groupLegend = legend.selectAll('g').data(labels).enter().append('g');
+        // add a group for each label needed
+        var groupLegend = legend.selectAll('g')
+            .data(labels).enter().append('g');
 
+        // append a top text to the legend
         groupLegend.append('text')
-            .text('Dominant religion')
-            .attr('class', 'legend')
+            .text(legendTopText)
+            .attr('class', 'legend');
 
+        // append a square to the legend
         groupLegend.append('rect')
             .attr('class', 'bar')
-            .attr('fill', function(d, i) { return colors[d]; })
-            .attr('y', function(d, i) { return 25 + (i * 25); })
-            .attr('transform', 'translate(2,0)')
-            .attr('width', 20)
-            .attr('height', 20);
 
+            // fill it with color associated with label
+            .attr('fill', function(d, i) { return colors[d]; })
+
+            // place it correctly
+            .attr('y', function(d, i) { return legendSpacing + (i * legendSpacing); })
+            .attr('transform', 'translate(2,0)')
+
+            // set width and height
+            .attr('width', legendBox)
+            .attr('height', legendBox);
+
+        // set text for each box
         groupLegend.append('text')
-            .attr('y', function(d, i) { return 42 + (i * 25); })
-            .attr('x', 30)
+            .attr('y', function(d, i) { return legendTextOffset + (i * legendSpacing); })
+            .attr('x', legendOffset + 10)
+
+            // make each first letter uppercase
             .text(function(d) { return d[0].toUpperCase() + d.substring(1); });
 
+        // place bottom text
         groupLegend.append('text')
             .attr('class', 'legend')
-            .attr('y', function() { return 25 + ((labels.length + 1) * 25) })
-            .text('Size represents population');
+            .attr('y', function() { return legendSpacing + ((labels.length + 1) * legendSpacing) })
+            .text(legendBotText);
 
+        // make title for plot
         svg.append('text')
             .attr('class', 'title')
             .attr('transform', 'translate(' + (margin.left + width / 2) + ',' + (margin.top / 2) + ')')
