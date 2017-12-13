2535783327c911da67ac1224bd8b355c468654df
Sebastiaan Arendsen <sa.arendsen@gmail.com>
2017-12-13 13:15:34 +0100

    donish

--- Homework/week_6/static/scripts.js
+++ Homework/week_6/static/scripts.js
@@ -1,7 +1,8 @@
-/* Sebastiaan Arendsen
+    /* Sebastiaan Arendsen
  * 6060072
  * Minor Programmeren - Data Processing
  */
+
 'use strict';
 
 const DATA1 = 'data/tweede_kamer_opkomst.csv',
@@ -10,9 +11,9 @@ const DATA1 = 'data/tweede_kamer_opkomst.csv',
 var marginsBar = {top: 20, right: 20, bottom: 30, left: 40},
     widthBar = 700,
     heightBar = 600,
-    marginsPie = {top: 20, right: 20, bottom: 30, left: 40},
-    widthPie = 300,
-    heightPie = 200,
+    marginsPie = {top: 20, right: 20, bottom: 30, left: 180},
+    widthPie = 700,
+    heightPie = 700,
     barChart,
     pieChart,
     xBar,
@@ -30,12 +31,13 @@ document.addEventListener('DOMContentLoaded', function() {
         .await(function(error, data1, data2) {
             if (error) throw error;
 
-            console.log(data1);
-            console.log(data2);
-
             stackedBarChartInit();
 
-            stackedBarChartFill(data1);
+            stackedBarChartFill(data1, data2);
+
+            pieChartInit();
+
+            pieChartUpdate(data2, 0);
 
         });
 
@@ -44,8 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function stackedBarChartInit() {
 
     // create an svg for the barchart
-    var svg = d3.select('body')
-        .append('svg')
+    var svg = d3.select('.bar')
         .attr('id', 'barsvg')
         .attr('width', widthBar + marginsBar.left + marginsBar.right)
         .attr('height', heightBar + marginsBar.top + marginsBar.bottom);
@@ -65,18 +66,15 @@ function stackedBarChartInit() {
         .range(['#98abc5', '#8a89a6']);
 }
 
-function stackedBarChartFill(data) {
+function stackedBarChartFill(data, json) {
 
     var keys = data.columns.slice(2);
 
-    console.log(keys);
     xBar.domain(data.map(function(d) { return d['Jaar']; }));
 
     yBar.domain([0, d3.max(data, function(d) { return d['Totaal stemmen']; })]);
 
     zBar.domain(keys);
-
-    console.log(d3.stack().keys(keys)(data));
 
     barChart.append('g')
         .selectAll('g')
@@ -86,7 +84,9 @@ function stackedBarChartFill(data) {
     .selectAll('rect')
         .data(function(d) { return d; })
     .enter().append('rect')
-        .on('mouseover', function(d) {  pieChartUpdate(d.data['Jaar']) })
+        .on('click', function(d,i) { return pieChartUpdate(json, i)})
+        .on('mouseover', function() { document.body.style.cursor = 'pointer';} )
+        .on('mouseout' , function() { document.body.style.cursor = 'default'; })
         .attr('x', function(d) { return xBar(d.data['Jaar']); })
         .attr('y', function(d) { return yBar(d[1]); })
         .attr('height', function(d) { return yBar(d[0]) - yBar(d[1]); })
@@ -100,34 +100,125 @@ function stackedBarChartFill(data) {
     barChart.append('g')
         .attr('class', 'axis')
         .call(d3.axisLeft(yBar).ticks(null, 's'));
+
+    var legend = d3.select('.bar').append('g')
+        .attr('transform', 'translate(60,' + (marginsBar.top + 20) + ')');
+        
+    // add a group for each label needed
+    var groupLegend = legend.selectAll('g')
+        .data(['Ongeldig', 'Geldig'])
+        .enter().append('g');
+
+    // add colored squares for the legend
+    groupLegend.append('rect')
+        .attr('width', '20')
+        .attr('height', '20')
+        .attr('fill', function(d) { return zBar(d); })
+        .attr('y', function(d, i) { return i * 25; });
+
+    // add text for the legend
+    groupLegend.append('text')
+        .attr('transform', 'translate(' + 20 + ',0)')
+        .attr('y', function(d, i) { return 15 + (i * 25); })
+        .attr('x', 10)
+        .text(function(d) { return d; });
 }
 
-function pieChartInit() {
-    var svg = d3.select('body')    
-        .append('svg')
-        .attr('id', 'piesvg')
-        .attr('width', widthBar + marginsBar.left + marginsBar.right)
-        .attr('height', heightBar + marginsBar.top + marginsBar.bottom);
+var pieChart,
+    pie,
+    color,
+    label,
+    path,
+    pieLegend;
 
-    var radius = Math.min(widthPie, heightPie) / 2
+function pieChartInit() {
+    var svg = d3.select('.pie')
+        .attr('width', widthPie + marginsPie.left + marginsPie.right)
+        .attr('height', heightPie + marginsPie.top + marginsPie.bottom);
+
+    var radius = Math.min(widthPie, heightPie) / 2;
+
+    pie = d3.pie()
+        .sort(null)
+        .value(function(d) { return d.zetels; });
 
     pieChart = svg.append('g')
-        .attr('transform', 'translate(' + (width / 2 + marginsPie.left) + ',' + (height / 2 + marginsPie.top) + ')')
+        .attr('transform', 'translate(' + (widthPie / 2 + marginsPie.left) + ',' + (heightPie / 2 + marginsPie.top) + ')');
 
-    var color = d3.scaleOrdinal(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    color = d3.scaleOrdinal(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
 
-    var path = d3.arc()
+    path = d3.arc()
         .outerRadius(radius - 10)
         .innerRadius(0);
 
-    var label = d3.arc()
+    label = d3.arc()
         .outerRadius(radius - 40)
         .innerRadius(radius - 40);
 
+    pieLegend = d3.select('.pie')
+        .append('g')
+        .attr('transform', 'translate(4,' + marginsPie.top + ')');
+
 }
 
-// function pieChartUpdate(data, year) {
 
-//     var pie = 
-    
-// }
+function pieChartUpdate(json, year) {
+
+    pieLegend.selectAll('g')
+        .remove();
+
+    var data = json[year];
+
+    for (var key in data) {
+        if (data[key] == undefined || key == 'Jaar' || data[key] == '-' || data[key].length === 0) {
+            delete data[key];
+        }
+
+        else if (key == 'Totaal aantal zetels') {
+            delete data[key];
+        }
+        else {
+            data[key] = +data[key];
+        }
+    }
+
+    var fitData = [];
+
+    for (var key in data) {
+        fitData.push({'partij': key,
+            'zetels': data[key]
+        });
+    }
+
+    pieChart.selectAll('.arc')
+        .remove();
+
+    var arc = pieChart.selectAll('.arc')
+        .data(pie(fitData))
+    .enter().append('g')
+        .attr('class', 'arc');
+
+    arc.append('path')
+        .attr('d', path)
+        .attr('fill', function(d) { return color(d.data['partij']); });
+
+    arc.append('text')
+        .attr('transform', function(d) { return 'translate(' + label.centroid(d) + ')'; })
+        .attr('dy', '0.35em')
+        .text(function(d) { return d.data['partij']; });
+
+    var groupLegend = pieLegend.selectAll('g')
+        .data(fitData).enter().append('g');
+
+    groupLegend.append('rect')
+        .attr('width', '20')
+        .attr('height', '20')
+        .attr('fill', function(d) { return color(d['partij']); })
+        .attr('y', function(d, i) { return i * 25; });
+
+    groupLegend.append('text')
+        .attr('transform', 'translate(' + 20 + ',0)')
+        .attr('y', function(d, i) { return 15 + (i * 25); })
+        .attr('x', 10)
+        .text(function(d) { return d['partij']; });
+}
